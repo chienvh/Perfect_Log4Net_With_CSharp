8b4da0ffd1181ae007fd574664c717de9fc0e215
Chien Vu <chien.vh@gmail.com>
2016-06-20 22:51:05 +0700

    Initial commit

---
+++ Log4Net.CSharp/Log4Net.CSharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using log4net;
+using System.Threading;
+
+namespace Log4Net.CSharp
+{
+    class Program
+    {
+        //Declare an instance for log4net
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            ImplementLoggingFuntion();
+        }
+
+        private static void ImplementLoggingFuntion()
+        {
+            /* We have 5 levels of log message. Let's test all.
+             *  FATAL
+                ERROR
+                WARN
+                INFO
+                DEBUG
+             */
+            var secs = 3;
+            Log.Fatal("Start log FATAL...");
+            Console.WriteLine("Start log FATAL...");
+            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
+
+            Log.Error("Start log ERROR...");
+            Console.WriteLine("Start log ERROR...");
+            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
+
+            Log.Warn("Start log WARN...");
+            Console.WriteLine("Start log WARN...");
+            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
+
+            Log.Info("Start log INFO...");
+            Console.WriteLine("Start log INFO...");
+            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
+
+            Log.Debug("Start log DEBUG...");
+            Console.WriteLine("Start log DEBUG...");
+            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
+
+            Console.WriteLine("Press any key to close the application");
+            Console.ReadKey();
+        }
+    }
+}
